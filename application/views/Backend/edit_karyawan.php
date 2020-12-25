3d68d25e698949777d3dda26c683fff179536776
kiranaywt <56025572+kiranaywt@users.noreply.github.com>
2020-12-25 09:51:35 +0700

    revisi logo edit karyawan
    revisi logo edit karyawan

--- application/views/Backend/edit_karyawan.php
+++ application/views/Backend/edit_karyawan.php
@@ -26,7 +26,7 @@ if (!$this->session->userdata('nama')) {
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="<?= base_url('Homepage') ?>" class="brand-link">
-                <img src="assets/Frontend_mobi/assets/images/amalialogo.png" alt="AdminLTE Logo"
+                <img src="<?= base_url() ?>assets/Frontend_mobi/assets/images/amalialogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light"><b>Amalia</b> Collection</span>
             </a>
