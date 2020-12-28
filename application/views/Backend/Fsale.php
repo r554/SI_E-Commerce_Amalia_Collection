9afab258c1ce9ec4ec089d2836c9a32bd8bb1a14
Ferdiansyah Rifqika Evanta <56006056+E41181181@users.noreply.github.com>
2020-12-29 06:44:42 +0700

    revisi typo

--- application/views/Backend/Fsale.php
+++ application/views/Backend/Fsale.php
@@ -293,7 +293,7 @@
             <!-- Modal content-->
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Konfirmasi Pembayaran</h4>
+                    <h4 class="modal-title">Ubah Waktu Promo</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>          
                 </div>
                 <div class="modal-body">
